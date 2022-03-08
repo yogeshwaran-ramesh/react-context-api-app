e5952d5161af22ab45fa4bc58b12dfc5411f5c14
Yogeshwaran Ramesh <yogeshwaranramesh@Yogeshwaran-RameshB580s-MacBook-Pro.local>
2022-03-08 15:56:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const ImagesContext = React.createContext([]);
 
 function App() {
+  const [images, setImages] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const updateImages = (data) => setImages(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImagesContext.Provider
+        value={{ loading, images, setLoading, updateImages }}
+      >
+        <ImagesView />
+      </ImagesContext.Provider>
+    </div>
+  );
+}
+
+function ImagesView() {
+  const { loading, images, updateImages, setLoading } =
+    React.useContext(ImagesContext);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    fetch("https://picsum.photos/v2/list?page=1&limit=9")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        updateImages(responseJson);
+        setLoading(false);
+      })
+      .catch((e) => console.log(e));
+  };
+  return (
+    <div className="flex-center flex-column">
+      <h1>Context API</h1>
+      <button className="btn" onClick={() => fetchImages()}>
+        Fetch Images
+      </button>
+      {loading && <h1>loading...</h1>}
+      <div className="flex-center image-wrapper">
+        {images.map(({ download_url, id, author }) => (
+          <img className="image" key={id} src={download_url} alt={author} />
+        ))}
+      </div>
     </div>
   );
 }
